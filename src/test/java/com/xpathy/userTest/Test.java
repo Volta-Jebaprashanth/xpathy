15d47c1dce753eb069383dd75a486a5f7f1b6c2b
vjeb2704 <volta.jebaprashanth@sysco.com>
2026-08-02 23:42:58 +0530

    test: add Tag.div usage example in Test class
    Added a new static variable `xpathy2` to demonstrate the usage of the `Tag` class for building XPath expressions.

--- src/test/java/com/xpathy/userTest/Test.java
+++ src/test/java/com/xpathy/userTest/Test.java
@@ -2,6 +2,7 @@ package com.xpathy.userTest;
 import com.xpathy.XPathy;
 import com.xpathy.Attribute;
 import com.xpathy.Or;
+import com.xpathy.Tag;
 
 public class Test {
 
@@ -9,6 +10,9 @@ public class Test {
             .and()
             .byText().not()
             .union(Or.equals("1"), Or.contains("2"));
+
+    
+    static XPathy xpathy2 = Tag.div.byText().contains("test");
     
     public static void main(String[] args) {
         System.out.println(xpathy.getXpath());
